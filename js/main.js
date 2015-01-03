fcc8d5be654c464dc1d852cac0a86518d57186dc
Ben Brouse <benjaminbrouse@gmail.com>
2015-01-02 19:37:21 -0500

    Scrapped the old navbar for a bootstrap navbar. Adjusted the background on the main background image.

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 	    {
 		  animateClass: 'animated',
 		  mobile: false,
-		  offset: 300
+		  offset: 150,
 		}
 	);
 	wow.init();
@@ -47,34 +47,6 @@ $(document).ready(function(){
 	        scrollTop: target_element.offset().top - $('.nav').height() - target_element.css("marginTop").replace('px', '')
 	    }, 900);
 	    return false;
-	});
-
-	//sticky nav bar
-	// grab the initial top offset of the navigation 
-   	var stickyNavTop = $('.nav').offset().top;
-   	
-   	// our function that decides weather the navigation bar should have "fixed" css position or not.
-   	var stickyNav = function(){
-	    var scrollTop = $(window).scrollTop(); // our current vertical position from the top
-
-	    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-	    // otherwise change it back to relative
-	    console.log("checking sticky nav")
-	    if (scrollTop > stickyNavTop) { 
-	        $('.nav').addClass('sticky');
-	    } else {
-	        $('.nav').removeClass('sticky'); 
-	    }
-	};
-
-	stickyNav();
-	// and run it again every time you scroll
-	$(window).scroll(function() {
-		stickyNav();
-	});
-
-	$( window ).resize(function() {
-		stickyNavTop = $(window).height()*.92
 	});
 
 });
